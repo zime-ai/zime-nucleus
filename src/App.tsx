91a40205b249f404745bbfed66d9f2a3d0d9f226
kartik-zime <kartik@zime.ai>
2025-04-16 01:52:57 +0530

    Add ESLint config and remove unnecessary React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { MeetingUploader } from './components/MeetingUploader';
 import { TranscriptUpdater } from './components/TranscriptUpdater';
 import { SpeakerNameUpdater } from './components/SpeakerNameUpdater';
